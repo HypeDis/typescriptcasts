a5f018f4c4498141c395e6864614c611b8f1ad95
hypedis <m.bae@live.com>
2019-09-24 22:38:14 -0400

    added google maps api

---
+++ maps/src/index.ts
@@ -0,0 +1,11 @@
+console.log('hi there!');
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+console.log(user);
+
+const company = new Company();
+console.log(company);
+
+google
