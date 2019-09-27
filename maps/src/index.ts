b2ac92d467096329e737390f4a911f68cdf81f0c
hypedis <m.bae@live.com>
2019-09-27 15:11:09 -0400

    finished map app

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,11 +1,9 @@
-console.log('hi there!');
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
-console.log(user);
-
 const company = new Company();
-console.log(company);
-
-google
+const customMap = new CustomMap('map');
+customMap.addMarker(user);
+customMap.addMarker(company);
